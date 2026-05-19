b5084df5726ad5d412d7d12d2c3ed18d57491a49
ClaudiuCristianRotaru <clayarname4@gmail.com>
2026-05-19 04:52:07 +0300

    feat: split message handling

--- Services/logger.py
+++ Services/logger.py
@@ -12,30 +12,80 @@ cred = credentials.Certificate(str(key_path))
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-topic = "nodes/+/telemetry/#"
+topic = "nodes/+/+/#"
 
-def save_to_firestore(node_id, data):
-    doc_ref = db.collection("telemetry").document(data['id'])
+def print_message(data):
+    print(f"node: {data.get("node")}")
+    print(f"type: {data.get("type")}")
+    print(f"status: { "✅" if data.get("status") == "ok" else "❌"}")
+    print(f"subject: {data.get("subject")}")
+    print(f"timestamp: {data.get("timestamp").strftime("%H:%M:%S")}")
+    print(f"payload: {data.get("payload")}")
+    print("\n")
+
+
+def save_to_firestore(collection_name, document_name, data):
+    doc_ref = db.collection(collection_name).document(document_name)
     
     doc_ref.set(data)
-    print(f"Data synced to Cloud for node {node_id}")
+    print(f"Data synced to Cloud")
+
+def update_node_status(node_name, data):
+    doc_ref = db.collection("nodes").document(node_name)
+    payload = data.get("payload")
+    node_status = {
+        "last_seen": data.get("timestamp"),
+        "status": data.get("status")
+    }
+    if payload.get("online"):
+        node_status["online"] = payload.get("online")
+
+    if payload.get("uptime"):
+        node_status["uptime"] = payload.get("uptime")
+
+    if payload.get("free_ram"):
+        node_status["free_ram"] = payload.get("free_ram")
+
+    doc_ref.set(node_status, merge= True)
 
 
 def on_message(client, userdata, message):
-    print(f"Received: {message.payload.decode()} on {message.topic}")
+    data = json.loads(message.payload.decode("utf-8"))
+    if "timestamp" in data:
+        data["timestamp"] = datetime.fromisoformat(data["timestamp"])
 
-    raw_data = json.loads(message.payload.decode("utf-8"))
-    if "timestamp" in raw_data:
-        raw_data["timestamp"] = datetime.fromisoformat(raw_data["timestamp"])
+    id = data.get("id")
+    type = data.get("type")
+    node = data.get("node")
+    subject = data.get("subject")
+    status = data.get("status")
+    payload = data.get("payload", {})
+    timestamp = data.get("timestamp")
 
-    node = raw_data["node"] if "node" in raw_data else "Unknown node"
+    type_to_collection = {
+        "telemetry": "telemetry",
+        "alert": "alerts",
+        "system_log": "system_logs"
+    }
 
-    save_to_firestore(node, raw_data)
+    collection_name = type_to_collection.get(type, "unknown")
+    document_name = id
 
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id="Test_Subscriber")
+    if status != "ok":
+        collection_name = "errors"
+    elif subject == "heartbeat":
+        print("------HEARTBEAT------")
+        update_node_status(node, data)
+        return
+    
+    if payload.get("online"):
+        update_node_status(node, data)
+        
+    save_to_firestore(collection_name, document_name, data)
+    print("saving to", collection_name, "document", document_name, "data", payload)
 
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id="logger_service")
 client.on_message = on_message
-
 client.connect("localhost", 1883)
 client.subscribe(topic)
 
